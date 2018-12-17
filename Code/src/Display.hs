ac7d0f5274cfa7b7939bdfab8ac17f578a34345a
Jack <jackellis1504@gmail.com>
2018-12-17 15:48:22 +0000

    Drawing a character to the screen

--- Code/src/Display.hs
+++ Code/src/Display.hs
@@ -4,4 +4,9 @@ import Graphics.UI.GLUT
 
 display :: DisplayCallback
 display = do clear [ColorBuffer]
+             translate $ Vector3 (-1.0) (-1.0) (0.0 :: Float)
+             scale (1.0/32.0) (1.0/24.0) (0.0 :: Float)
+             color $ Color3 0.0 1.0 (0.0 :: Float)
+             let p = [(1,0,0),(1,1,0),(0,1,0),(0,0,0)] :: [(Float,Float,Float)]
+             renderPrimitive Quads $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) p
              flush
