73f3b6cc1f631f3e0532fabf615db7d15d8aa8e4
Jack <jackellis1504@gmail.com>
2018-12-17 17:50:37 +0000

    Now displaying a unit square - the Frogger

--- Code/src/Display.hs
+++ Code/src/Display.hs
@@ -2,11 +2,13 @@ module Display (display) where
 
 import Graphics.UI.GLUT
 
-display :: DisplayCallback
 display = do clear [ColorBuffer]
              translate $ Vector3 (-1.0) (-1.0) (0.0 :: Float)
-             scale (1.0/32.0) (1.0/24.0) (0.0 :: Float)
-             color $ Color3 0.0 1.0 (0.0 :: Float)
-             let p = [(1,0,0),(1,1,0),(0,1,0),(0,0,0)] :: [(Float,Float,Float)]
-             renderPrimitive Quads $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) p
+             scale (1.0/320.0) (1.0/240.0) (0.0 :: Float)
+             preservingMatrix $ do color $ Color3 0.0 1.0 (0.0 :: Float)
+                                   let p = [(10, 0,  0),
+                                            (10, 10, 0),
+                                            (0 , 10, 0),
+                                            (0 , 0,  0)] :: [(Float,Float,Float)]
+                                   renderPrimitive Quads $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) p
              flush
