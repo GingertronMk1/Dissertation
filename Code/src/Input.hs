cd5dd782916f0c01ad60136571456f593e1bf61b
Jack <jackellis1504@gmail.com>
2018-12-18 15:02:43 +0000

    Changes to allow for multiple objects on screen
    - Changed `input` to accept an Env
    - Modified `input` to modify a nested record-style syntax

--- Code/src/Input.hs
+++ Code/src/Input.hs
@@ -4,11 +4,16 @@ import Graphics.UI.GLUT
 import Data.IORef
 import Type
 
-input :: IORef Mover -> KeyboardMouseCallback
-input m c Down _ _ | c == (Char 'w') || c == (Char 'W') = m $~! \f -> f {y = y f + step}
-                   | c == (Char 'a') || c == (Char 'A') = m $~! \f -> f {x = x f - step}
-                   | c == (Char 's') || c == (Char 'S') = m $~! \f -> f {y = y f - step}
-                   | c == (Char 'd') || c == (Char 'D') = m $~! \f -> f {x = x f + step}
-                   | otherwise                          = return ()
-                   where step = 5
+input :: IORef Env -> KeyboardMouseCallback
+input m c Down _ _
+  | c == (Char 'w') || c == (Char 'W') = m $~! \e -> let p = player e
+                                                     in e {player = p {y = y p + step}}
+  | c == (Char 'a') || c == (Char 'A') = m $~! \e -> let p = player e
+                                                     in e {player = p {x = x p - step}}
+  | c == (Char 's') || c == (Char 'S') = m $~! \e -> let p = player e
+                                                     in e {player = p {y = y p - step}}
+  | c == (Char 'd') || c == (Char 'D') = m $~! \e -> let p = player e
+                                                     in e {player = p {x = x p + step}}
+  | otherwise                          = return ()
+  where step = 5
 input _ _ _ _ _ = return ()
