3b5337987fb375493682f0d8822d6878aca9c0a6
Jack <jackellis1504@gmail.com>
2018-12-18 14:59:57 +0000

    Changes to allow for multiple objects on screen
    - Moved idle to its own module
    - Replaced the Frogger with an empty environment IORef

--- Code/src/Main.hs
+++ Code/src/Main.hs
@@ -3,8 +3,9 @@ module Main where
 import Graphics.UI.GLUT
 import Data.IORef
 import Display
-import Type
+import Idle
 import Input
+import Type
 
 main :: IO()
 main = do (_progName, _args) <- getArgsAndInitialize
@@ -13,14 +14,12 @@ main = do (_progName, _args) <- getArgsAndInitialize
           initialWindowSize $= Size 640 480
           createWindow "Frogger"
           reshapeCallback $= Just reshape
-          f <- newIORef $ Frogger {x = 320.0, y = 240.0, s = 10.0}
+          f <- newIORef $ emptyEnv
           keyboardMouseCallback $= Just (input f)
           displayCallback $= display f
           idleCallback $= Just (idle f)
           mainLoop
 
-idle e = do e $~! id
-            postRedisplay Nothing
 
 reshape size = do viewport $= (Position 0 0, size)
                   postRedisplay Nothing
