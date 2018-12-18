8b79881890c46a5bfeee7ba467c5918dea2e8f0a
Jack <jackellis1504@gmail.com>
2018-12-18 15:01:44 +0000

    Changes to allow for multiple objects on screen
    - Changed `display` to take an Env rather than a Mover
    - Pulled the square-drawing code out to its own function
    - Added a function for converting a 3-tuple to a vertex

--- Code/src/Display.hs
+++ Code/src/Display.hs
@@ -4,17 +4,32 @@ import Graphics.UI.GLUT
 import Data.IORef
 import Type
 
-display :: IORef Mover -> DisplayCallback
+display :: IORef Env -> DisplayCallback
 display m = do clear [ColorBuffer]
                loadIdentity
                fr <- get m
-               putStrLn $ show fr
                translate $ Vector3 (-1.0) (-1.0) (0.0 :: Float)
                scale (1.0/320.0) (1.0/240.0) (0.0 :: Float)
-               preservingMatrix $ do color $ Color3 0.0 1.0 (0.0 :: Float)
-                                     let p = [(x fr + s fr,y fr,0),
-                                               (x fr + s fr,y fr + s fr,0),
-                                               (x fr,y fr + s fr,0),
-                                               (x fr,y fr,0)] :: [(Float,Float,Float)]
-                                     renderPrimitive Quads $ mapM_ (\(x,y,z) -> vertex $ Vertex3 x y z) p
+               (drawMover . player) fr
+               (sequence . map drawMover . enemies) fr
                swapBuffers
+
+drawMover :: Mover -> IO()
+drawMover m = preservingMatrix $ drawMover' m
+
+drawMover' :: Mover -> IO()
+drawMover' Frogger {x = fx, y = fy, s = fs} = do color $ Color3 0.0 1.0 (0.0 :: Float)
+                                                 translate $ Vector3 fx fy 0.0
+                                                 scale fs fs 1.0
+                                                 unitSquare
+drawMover' Car {x = cx, y = cy, l = cl, w = cw} = do color $ Color3 1.0 0.0 (0.0 :: Float)
+                                                     translate $ Vector3 cx cy 0.0
+                                                     scale cl cw 1.0
+                                                     unitSquare
+
+unitSquare :: IO()
+unitSquare = let us = [(1,0,0),(1,1,0),(0,1,0),(0,0,0)] :: [(Float, Float, Float)]
+             in (renderPrimitive Quads . mapM_ makeVertex) us
+
+makeVertex :: (Float, Float, Float) -> IO()
+makeVertex (x,y,z) = vertex $ Vertex3 x y z
