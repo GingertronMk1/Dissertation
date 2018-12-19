c825216c1293cd95bcfc0b91a713f2f4406f4540
Jack <jackellis1504@gmail.com>
2018-12-19 15:27:19 +0000

    Updated for new mover types as well as game state

--- Code/src/Idle.hs
+++ Code/src/Idle.hs
@@ -5,12 +5,13 @@ import Data.IORef
 import Type
 
 idle :: IORef Env -> IdleCallback
-idle e = do e $~! \env -> let es = enemies env
-                          in env {enemies = map updateMover es}
-            e' <- get e
+idle e = do e' <- get e
             hitCheck e'
-            --putStrLn $ show e'
-            postRedisplay Nothing
+            if gameState e' == Playing
+            then do e $~! \env -> let es = enemies env
+                                  in env {enemies = map updateMover es}
+                    postRedisplay Nothing
+            else postRedisplay Nothing
 
 hitCheck :: Env -> IO()
 hitCheck env = if or $ map (hasCollided (player env)) (enemies env)
@@ -19,8 +20,12 @@ hitCheck env = if or $ map (hasCollided (player env)) (enemies env)
 
 updateMover :: Mover -> Mover
 updateMover c@(Car {x = cx, v = cv}) = c {x = cx + cv}
+updateMover c@(Croc {x = cx, v = cv}) = c {x = cx + cv}
+updateMover t@(Turtles{x = tx, v = tv}) = t {x = tx + tv}
+updateMover l@(Log{x = lx, v = lv}) = l {x = lx + lv}
 
 hasCollided :: Mover -> Mover -> Bool
+{-
 hasCollided (Frogger {x=fx, y=fy, s=fs}) (Car {x=cx, y=cy, l=cl, w=cw, v=cv})
   = let froggerBoundsX = (fx,fx+fs)
         froggerBoundsY = (fy,fy+fs)
@@ -28,6 +33,14 @@ hasCollided (Frogger {x=fx, y=fy, s=fs}) (Car {x=cx, y=cy, l=cl, w=cw, v=cv})
         carBoundsY     = (cy,cy+cw)
     in (inRange froggerBoundsX (fst carBoundsX) || inRange froggerBoundsX (snd carBoundsX))
        && (inRange froggerBoundsY (fst carBoundsY) || inRange froggerBoundsY (snd carBoundsY))
+       -}
+hasCollided f m = let froggerBoundsX = (x f, x f + s f)
+                      froggerBoundsY = (y f, y f + s f)
+                      moverBoundsX     = (x m, x m+(l m*(signum (v m))))
+                      moverBoundsY     = (y m, y m+w m)
+                  in (inRange froggerBoundsX (fst moverBoundsX) || inRange froggerBoundsX (snd moverBoundsX))
+                  && (inRange froggerBoundsY (fst moverBoundsY) || inRange froggerBoundsY (snd moverBoundsY))
+
 
 inRange :: (Num a, Ord a) => (a, a) -> a -> Bool
 inRange (m, n) p = if m < n then m <= p && p <= n
