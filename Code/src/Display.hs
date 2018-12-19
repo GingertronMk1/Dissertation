720ee706c2e8e33d3524d4cffee119b172c075c5
Jack <jackellis1504@gmail.com>
2018-12-19 13:37:42 +0000

    Some major changes
    - Now drawing road and river lanes
    - Using an infinite list to generate the lane coordinates in y
    - Gave the cars windscreens :)
    - Drawing the player on top of everything else

--- Code/src/Display.hs
+++ Code/src/Display.hs
@@ -10,8 +10,11 @@ display m = do clear [ColorBuffer]
                fr <- get m
                translate $ Vector3 (-1.0) (-1.0) (0.0 :: Float)
                scale (1.0/320.0) (1.0/240.0) (0.0 :: Float)
-               (drawMover . player) fr
+               let lanes = [0.0,32.0..] :: [Float]
+               (sequence . map drawRoadLane . take 5 . drop 1) lanes
+               (sequence . map drawRiverLane . take 5 . drop 7) lanes
                (sequence . map drawMover . enemies) fr
+               (drawMover . player) fr
                swapBuffers
 
 drawMover :: Mover -> IO()
@@ -22,10 +25,27 @@ drawMover' Frogger {x = fx, y = fy, s = fs} = do color $ Color3 0.0 1.0 (0.0 :: 
                                                  translate $ Vector3 fx fy 0.0
                                                  scale fs fs 1.0
                                                  unitSquare
-drawMover' Car {x = cx, y = cy, l = cl, w = cw} = do color $ Color3 1.0 0.0 (0.0 :: Float)
-                                                     translate $ Vector3 cx cy 0.0
-                                                     scale cl cw 1.0
-                                                     unitSquare
+drawMover' Car {x = cx, y = cy, l = cl, w = cw, v = cv} = do color $ Color3 1.0 0.0 (0.0 :: Float)
+                                                             translate $ Vector3 cx cy 0.0
+                                                             scale (cl*(signum cv)) cw 1.0
+                                                             unitSquare
+                                                             color $ Color3 0.3 0.3 (1.0 :: Float)
+                                                             translate $ Vector3 0.8 0.0 (0.0 :: Float)
+                                                             scale 0.1 1.0 (1.0 :: Float)
+                                                             unitSquare
+
+drawRiverLane :: Float -> IO()
+drawRiverLane y = preservingMatrix $ do color $ Color3 0.2 0.2 (1.0 :: Float)
+                                        drawLane y
+
+drawRoadLane :: Float -> IO()
+drawRoadLane y = preservingMatrix $ do color $ Color3 0.3 0.3 (0.3 :: Float)
+                                       drawLane y
+
+drawLane :: Float -> IO()
+drawLane y = do translate $ Vector3 0.0 y 0.0
+                scale 640.0 30.0 (1.0 :: Float)
+                unitSquare
 
 unitSquare :: IO()
 unitSquare = let us = [(1,0,0),(1,1,0),(0,1,0),(0,0,0)] :: [(Float, Float, Float)]
