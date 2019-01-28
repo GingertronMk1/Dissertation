8d9462870f47a584b3fafbfa39267703d84bbc49
Jack <jackellis1504@gmail.com>
2019-01-28 17:14:04 +0000

    Updated to make use of the new Drawable typeclass
    - Using the draw operation that's been moved into Type.hs
    - Adjusted display to draw different things based on the current gameState

--- Code/src/Display.hs
+++ Code/src/Display.hs
@@ -6,27 +6,43 @@ import Data.List (intersperse)
 import Type
 
 display :: IORef Env -> DisplayCallback
-display m = do clear [ColorBuffer]
+display m = do fr <- get m
+               clear [ColorBuffer]
                loadIdentity
                translate $ Vector3 (-1.0) (-1.0) (0.0 :: Float)
-               scale (1.0/320.0) (1.0/240.0) (0.0 :: Float)
-               fr <- get m
-               t <- get elapsedTime
-               ppStats fr
-               let lanes = [0.0,32.0..] :: [Float]
-               (sequence . map drawRoadLane . take 5 . drop 1) lanes
-               (sequence . map drawRiverLane . take 5 . drop 7) lanes
-               --(sequence . map drawMover . enemies) fr
-               --(sequence . map drawMover . goals) fr
-               --(drawMover . player) fr
-               (sequence . map draw) (player fr : (enemies fr ++ goals fr))
-               m $~! \e -> e {frames = frames e + 1, time = t}
+               scale (2.0/initSizeX) (2.0/initSizeY) (0.0 :: Float)
+               case (gameState fr) of PlayerDead _  -> do let cause = ((\(PlayerDead s) -> s) . gameState) fr
+                                                              score = "You died with " ++ show (gameScore fr) ++ " points!"
+                                                              space = "Press space to play again!"
+                                                          color $ Color3 1.0 1.0 (1.0 :: Float)
+                                                          preservingMatrix $ do translate $ Vector3 32.0 240.0 (0.0 :: Float)
+                                                                                scale 0.1 0.1 (1.0 :: Float)
+                                                                                preservingMatrix $ renderString MonoRoman $ cause
+                                                                                translate $ Vector3 0.0 (-200.0) (0.0 :: Float)
+                                                                                preservingMatrix $ renderString MonoRoman $ score
+                                                                                translate $ Vector3 0.0 (-200.0) (0.0 :: Float)
+                                                                                preservingMatrix $ renderString MonoRoman $ space
+                                      LevelComplete -> do let score = show $ gameScore fr
+                                                              state = "You completed the level with " ++ score ++ " points!"
+                                                          color $ Color3 1.0 1.0 (1.0 :: Float)
+                                                          preservingMatrix $ do translate $ Vector3 (32.0) 240.0 (0.0 :: Float)
+                                                                                scale 0.1 0.1 (1.0 :: Float)
+                                                                                renderString MonoRoman $ state
+                                      otherwise     -> do t <- get elapsedTime
+                                                          ppStats fr
+                                                          (sequence . map drawRoadLane . take 5) lanes
+                                                          (sequence . map drawRiverLane . take 5 . drop 6) lanes
+                                                          (preservingDraws . roadEnemies) fr
+                                                          (preservingDraws . riverEnemies) fr
+                                                          (preservingDraws . goals) fr
+                                                          (preservingDraw . player) fr
+                                                          m $~! \e -> e {frames = frames e + 1, time = t}
                swapBuffers
 
 ppStats :: Env -> IO()
 ppStats e = let t = time e
                 ppFrog = show $ player e
-                ppMovers = (concat . intersperse "\n" . map (("  "++) . show) . enemies) e
+                ppMovers = (concat . intersperse "\n" . map (("  "++) . show) . roadEnemies) e
                 ppFPS = if t > 0 then show $ div (frames e * 1000) t else "0"
                 ppState = show $ gameState e
                 ppScore = show $ gameScore e
@@ -38,61 +54,6 @@ ppStats e = let t = time e
                                       translate $ Vector3 200.0 0.0 (0.0 :: Float)
                                       preservingMatrix $ do scale 0.1 0.1 (1.0 :: Float)
                                                             renderString MonoRoman $ ppScore
-                                      putStr "\ESC[2J"
-                                      (putStrLn . concat . intersperse "\n") [ppFrog,"Movers",ppMovers,ppFPS,ppState,ppTime]
-
-drawMover :: Mover -> IO()
-drawMover m = preservingMatrix $ drawMover' m
-
-drawMover' :: Mover -> IO()
-drawMover' Frogger {x = fx, y = fy, s = fs} = do color $ Color3 0.0 1.0 (0.0 :: Float)
-                                                 translate $ Vector3 fx fy 0.0
-                                                 scale fs fs 1.0
-                                                 unitSquare
-drawMover' Car {x = cx, y = cy, l = cl, w = cw, v = cv} = do color $ Color3 1.0 0.0 (0.0 :: Float)
-                                                             translate $ Vector3 cx cy 0.0
-                                                             scale cl cw 1.0
-                                                             unitSquare
-                                                             color $ Color3 0.3 0.3 (1.0 :: Float)
-                                                             translate $ Vector3 0.8 0.0 (0.0 :: Float)
-                                                             scale 0.1 1.0 (1.0 :: Float)
-                                                             unitSquare
-drawMover' Croc {x = cx, y = cy, l = cl, w = cw, v = cv} = do color $ Color3 0.0 0.5 (0.0 :: Float)
-                                                              translate $ Vector3 cx cy 0.0
-                                                              scale cl cw 1.0
-                                                              unitSquare
-                                                              color $ Color3 1.0 1.0 (1.0 :: Float)
-                                                              preservingMatrix $ do translate $ Vector3 0.8 (0.1) (0.0 :: Float)
-                                                                                    scale 0.1 0.2 (1.0 :: Float)
-                                                                                    unitSquare
-                                                              preservingMatrix $ do translate $ Vector3 0.8 (0.8) (0.0 :: Float)
-                                                                                    scale 0.1 0.2 (1.0 :: Float)
-                                                                                    unitSquare
-drawMover' Turtles {x = tx, y = ty, l = tl, w = tw, v = tv} = do translate $ Vector3 tx ty 0.0
-                                                                 scale (tw * signum tv) tw 1.0
-                                                                 drawTurtles
-drawMover' Log {x = lx, y = ly, l = ll, w = lw, v = lv} = do color $ Color3 0.6 0.3 (0.2 :: Float)
-                                                             translate $ Vector3 lx ly 0.0
-                                                             scale ll lw 1.0
-                                                             unitSquare
-drawMover' Goal {x = gx, y = gy, s = gs} = do color $ Color3 0.8 0.7 (0.2 :: Float)
-                                              translate $ Vector3 gx gy 0.0
-                                              scale gs gs 1.0
-                                              unitSquare
-
-
-drawTurtle = do preservingMatrix $ do translate $ Vector3 0.1 0.1 (0.0 :: Float)
-                                      scale 0.8 0.8 (1.0 :: Float)
-                                      color $ Color3 0.0 1.0 (0.0 :: Float)
-                                      unitSquare
-                color $ Color3 1.0 0.6 (0.0 :: Float)
-                unitCircle
-
-drawTurtles = do drawTurtle
-                 translate $ Vector3 1.0 0.0 (0.0 :: Float)
-                 drawTurtle
-                 translate $ Vector3 1.0 0.0 (0.0 :: Float)
-                 drawTurtle
 
 drawRiverLane :: Float -> IO()
 drawRiverLane y = preservingMatrix $ do color $ Color3 0.2 0.2 (1.0 :: Float)
@@ -106,18 +67,6 @@ drawLane :: Float -> IO()
 drawLane y = do translate $ Vector3 0.0 y 0.0
                 scale 640.0 30.0 (1.0 :: Float)
                 unitSquare
-
-unitCircle :: IO()
-unitCircle = let n = 50.0
-                 points = [(0.5*(sin (2*pi*k/n)+1.0), 0.5*(cos (2*pi*k/n)+1.0), 0) | k <- [1..n]]
-              in (renderPrimitive Polygon . mapM_ makeVertex) points
-
-unitSquare :: IO()
-unitSquare = let us = [(1,0,0),(1,1,0),(0,1,0),(0,0,0)] :: [(Float, Float, Float)]
-             in (renderPrimitive Quads . mapM_ makeVertex) us
-
-makeVertex :: (Float, Float, Float) -> IO()
-makeVertex (x,y,z) = vertex $ Vertex3 x y z
-
-instance Drawable Mover where
-  draw = drawMover
+                where unitSquare = let us = [(1,0,0),(1,1,0),(0,1,0),(0,0,0)] :: [(Float, Float, Float)]
+                                  in (renderPrimitive Quads . mapM_ makeVertex) us
+                      makeVertex (x,y,z) = vertex $ Vertex3 x y z
