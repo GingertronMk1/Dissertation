b0d1706b0efff98904c597e0bf5aeae2b3a7a511
Jack <jackellis1504@gmail.com>
2019-01-29 10:19:23 +0000

    Changes to promote readability
    - Adjusted indentation in some areas
    - Moved inRange to a where clause in hitCheck
    - A couple of cases of "let y = f(x) in y" --> "f(x)"
    - Removed endLevel function
    - Changed how the Frogger gets its velocity updated

--- Code/src/Idle.hs
+++ Code/src/Idle.hs
@@ -4,15 +4,15 @@ import Graphics.UI.GLUT
 import Data.IORef
 import Type
 
--- TODO: Use Either to feed back either just the game state or the game state plus the new velocity of the frogger if it's on the back of something
-
 idle :: IORef Env -> IdleCallback
 idle e = do e' <- get e
-            case (gameState e') of Playing -> e $~! \_ -> updateEnv e'
+            case (gameState e') of Playing -> e $~! \e' -> updateEnv e'
                                    otherwise -> return ()
             postRedisplay Nothing
 
-updateEnv e = let roadEnemies' = map update (roadEnemies e)
+updateEnv :: Env -> Env
+updateEnv e = let frogger' = update (player e)
+                  roadEnemies' = map update (roadEnemies e)
                   riverEnemies' = map update (riverEnemies e)
                   (f_V', gameState') = case hitCheck e of Left gs   -> (0.0, gs)
                                                           Right vgs -> vgs
@@ -20,46 +20,36 @@ updateEnv e = let roadEnemies' = map update (roadEnemies e)
                                 in if gameState' == LevelComplete
                                       then s + 1000
                                    else s
-                  frogger' = player e
-               in e {player = frogger' {f_X = f_X frogger' + f_V'
-                                       ,f_V = f_V'}
+               in e {player = frogger' {f_V = f_V'}
                     ,roadEnemies = roadEnemies'
                     ,riverEnemies = riverEnemies'
                     ,gameState = gameState'
                     ,gameScore = gameScore'
                     }
 
-endLevel :: Env -> Env
-endLevel e = e {gameState = LevelComplete
-               ,gameScore = gameScore e + 1000}
-
 hitCheck :: Env -> Either GameState (Float, GameState)
 hitCheck e = let frogger = player e
                  fx = f_X frogger
                  fy = f_Y frogger
               in if inRange (0,initSizeX) fx && inRange (0,initSizeY) fy
-                 then if inRange ((head lanes),(lanes !! 4)) fy -- On the road, death is the edge case
+                 then if inRange ((head lanes),(lanes !! 4)) fy       -- On the road, death is the edge case
                          then let collisions = map (roadCollision frogger) (roadEnemies e)
-                                  newState = roadCheck' collisions
-                               in newState
+                               in roadCheck' collisions
                       else if inRange ((lanes !! 6),(lanes !! 11)) fy -- On the river, death is the default case
                          then let collisions = map (riverCollision frogger) (riverEnemies e)
-                                  newState = riverCheck' collisions
-                               in newState
+                               in riverCheck' collisions
                       else let collisions = map (goalCollision frogger) (goals e)
                             in if or collisions then Left LevelComplete
                                                 else Left Playing
-                 else Left (PlayerDead "You moved out of bounds!")
-                 where roadCheck' [] = Left Playing
+                 else Left $ PlayerDead "You moved out of bounds!"
+                 where roadCheck' []     = Left Playing
                        roadCheck' (s:ss) = case s of PlayerDead _ -> Left s
                                                      otherwise    -> roadCheck' ss
-                       riverCheck' [] = Left (PlayerDead "You drowned!")
+                       riverCheck' []     = Left (PlayerDead "You drowned!")
                        riverCheck' (s:ss) = case s of Right _ -> s
                                                       Left (PlayerDead "You got eaten by a croc!") -> s
                                                       otherwise -> riverCheck' ss
-
-inRange :: Ord a => (a,a) -> a -> Bool
-inRange (l,u) n = u >= n && n >= l
+                       inRange (l,u) n = u >= n && n >= l
 
 roadCollision :: Frogger -> RoadMover -> GameState
 roadCollision (Frogger {f_X = fx, f_Y = fy, f_S = fs}) (Car {ro_X = cx, ro_Y = cy, ro_L = cl, ro_W = cw})
